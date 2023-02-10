4c4eb063e9a21235ca45ca0ee7fb8008a910a3cb
P Sathya Narayan <sathyanmbpk@gmail.com>
2023-02-11 00:43:53 +0530

    reg

--- app.py
+++ app.py
@@ -27,22 +27,27 @@ def home():
 # registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    
+    return render_template('register.html')
+    
+
+# registration success page
+@app.route('/register_success',methods=['GET', 'POST'])
+def register_success():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        registration_data.append((name, email))
-        return redirect(url_for('register_success'))
-    return render_template('register.html')
-
-# registration success page
-@app.route('/register_success')
-def register_success():
+        event = request.form['event']
+        registration_data.append(name)
+        registration_data.append(email)
+        registration_data.append(event)
+        print(registration_data)
     return 'Registration successful. Thank you for registering!'
 
 # admin page
-@app.route('/admin')
+@app.route('/admin',methods=['GET', 'POST'])
 def admin():
-    return render_template('admin.html', registration_data=registration_data)
+    return render_template('admin.html', name = registration_data[0], email = registration_data[1],event = registration_data[2])
 
 if __name__ == '__main__':
     app.run(debug=True)
